54cb4b103912c1cb40de4419deb0fc7d9ac1cecd
Nick Andriam <raza.jedidia@gmail.com>
2025-05-25 13:47:55 +0300

    refactor: remove unused imports from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
-import FloatingButton from "./components/floating-button/FloatingButton";
 import ScoreBoard from "./components/score-board/score-board";
-import Test from "./components/test";
 
 function App() {
   return <ScoreBoard />;
