8c1172293d28986fbe975eb11ce79d01ed53579c
Nick Andriam <raza.jedidia@gmail.com>
2025-05-25 13:33:24 +0300

    Chore: initialize project structure with main components and styles

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import FloatingButton from "./components/floating-button/FloatingButton";
+import ScoreBoard from "./components/score-board/score-board";
+import Test from "./components/test";
+
+function App() {
+  return <ScoreBoard />;
+}
+
+export default App;
