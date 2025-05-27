d833a8e24f6731015c6cf8fc2bbb967207c21f91
Nick Andriam <raza.jedidia@gmail.com>
2025-05-27 23:50:59 +0300

    feat: rename project to ScoreBoard and update manifest
    - Updated manifest.json to reflect new project name and author.
    - Changed description to "Advanced ScoreBoard".
    - Added icons for the PWA.
    - Added new PWA icons (192x192 and 512x512).
    - Configured Vite to use PWA plugin with appropriate settings.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,37 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "ScoreBoard App",
+        short_name: "ScoreBoard",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
