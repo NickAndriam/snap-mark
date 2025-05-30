c5e4d69c31ec7cffe00295e8232c9a1ff3fab563
Nick Andriam <raza.jedidia@gmail.com>
2025-05-30 17:11:43 +0300

    feat: update App layout and styles for improved responsiveness and aesthetics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import RotateScreen from "./components/score-board/rotate-screen";
 import ScoreBoard from "./components/score-board/score-board";
+import Settings from "./components/score-board/settings/settings";
 
 // bg-[#232525]
 
@@ -8,8 +9,9 @@ function App() {
   return (
     <>
       <RotateScreen />
-      <div className="relative hidden sm:grid md:grid lg:grid bg-black w-screen h-full text-white select-none">
+      <div className="relative hidden sm:grid md:grid lg:grid bg-black w-screen h- overflow-hidden text-white select-none">
         <ScoreBoard />
+        <Settings />
       </div>
     </>
   );
